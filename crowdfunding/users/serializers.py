e3d12d476d306d95aafbe2db38b01e6fbd97f823
Sarahbrod <102585340+Sarahbrod@users.noreply.github.com>
2022-09-20 13:31:30 +1000

    h404

--- crowdfunding/users/serializers.py
+++ crowdfunding/users/serializers.py
@@ -1,6 +1,7 @@
 from rest_framework import serializers
 from .models import CustomUser
 from django.contrib.auth.hashers import make_password
+from django.http import Http404, HttpResponseBadRequest
 
 
 class CustomUserSerializer(serializers.Serializer):
